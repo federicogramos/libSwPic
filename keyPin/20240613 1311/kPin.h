fc7fcaeb8daaaac0b59c228db0186dab5a5486ff
Federico Ramos <federico.g.ramos@gmail.com>
2025-04-01 13:44:11 -0700

    Update kPin.h

--- keyPin/20240613 1311/kPin.h
+++ keyPin/20240613 1311/kPin.h
@@ -1,7 +1,7 @@
 //==============================================================================
-// ARCHIVO: kPin.h
-// AUTOR: Federico Ramos
-// ULT MODIF: 2024-06-12
+// Archivo: kPin.h
+// Autor: Federico Ramos
+// Modificado 20250104 0136
 //==============================================================================
 
 
@@ -10,66 +10,73 @@
 
 
 //==============================================================================
-// NOMBRES DE PULSADORES POR PARTE DEL USUARIO
+// Nombres de pulsadores por parte del usuario.
+//==============================================================================
+
 #include <kPin_aliases.h>
-//==============================================================================
 
 
 //==============================================================================
-// ESTRUCTURAS
-//==============================================================================
+// Estructuras
 // No permite hacer arreglos de bitfields, por eso debo hacer estructura de
 // bitfield y luego un arreglo de esa estructura.
+//==============================================================================
 
 typedef struct
     {
-    unsigned char kId:5;
-    unsigned char flag:1;
-    unsigned char flanco:1;
-    }t_kEvent;
+    unsigned char kId : 5;
+    unsigned char flag : 1;
+    unsigned char flanco : 1;
+    } t_kEvent;
 
 typedef struct
     {
-    unsigned char state:1;
-    unsigned char prevState:1;
+    unsigned char state : 1;
+    unsigned char prevState : 1;
     unsigned char timer;
-    }t_kPin;
+    } t_kPin;
+
 
 
 //==============================================================================
-// PINDEF E INICIALIZACI”N DE VECTOR
+// Pindef e inicializaci√≥n de vector.
+//==============================================================================
+
 #include <kPin_pindef.h>
-//==============================================================================
 
 
 //==============================================================================
-// ENUMS
+// Enum
+//==============================================================================
+
 enum e_flanco {FLANCO_N,FLANCO_P};
-//==============================================================================
 
 
 //==============================================================================
-// DECLARACI”N DE VARIABLES GLOBALES
+// Globals
+//==============================================================================
+
 extern t_kEvent kPinBuffer;
-//==============================================================================
 
 
 //==============================================================================
-// MACROS
+// Macros
+//==============================================================================
+
 #define KPIN_EVENT() (kPinBuffer.flag?!(kPinBuffer.flag=0):0)
 #define KPIN_GET_ID() (kPinBuffer.kId)
 #define KPIN_EVENT_ID(i) (kPinBuffer.flag && kPinBuffer.kId==i?!(kPinBuffer.flag=0):0)
 #define KPIN_EVENT_FP_ID(i) (kPinBuffer.flag && kPinBuffer.flanco==FLANCO_P && kPinBuffer.kId==i?!(kPinBuffer.flag=0):0)
 #define KPIN_EVENT_FN_ID(i) (kPinBuffer.flag && kPinBuffer.flanco==FLANCO_N && kPinBuffer.kId==i?!(kPinBuffer.flag=0):0)
-//==============================================================================
 
 
 //==============================================================================
-// DECLARACION DE FUNCIONES
+// Prototipos
+//==============================================================================
+
 void kPin_driverDc(char pi, char kCurrLogState);
 void kPin_driverAcFn(char pi, char flanco, char kCurrLogState);
 void kPin_driverAcFp(char pi, char flanco, char kCurrLogState);
-//==============================================================================
 
 
-#endif//__K_PIN_H
+#endif// __K_PIN_H
