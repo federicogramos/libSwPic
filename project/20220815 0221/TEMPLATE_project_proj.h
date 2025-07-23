ba0ae92bfd5f715560b746c6d25156ba3cbcb339
Federico Ramos <federico.g.ramos@gmail.com>
2025-07-23 11:40:00 -0700

    project_userSettings changed a _proj nada mas

---
+++ project/20220815 0221/TEMPLATE_project_proj.h
@@ -0,0 +1,11 @@
+/*******************************************************************************
+ * @file /project/project_proj.h
+ * @author Federico Ramos <federico.g.ramos@gmail.com>
+ * @version  20250718 1248
+ * @note Cada proyecto particular tiene su propio archivo "project_proj.c".
+
+ * Cada proyecto puede tener su project_proj.h particular. Constantes, seteos es
+ * pecificos o cualquier definicion global al proyecto puede venir aqui.
+ ******************************************************************************/
+
+
