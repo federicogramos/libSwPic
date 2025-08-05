0ac1f7179986d279f5f520152d4695663f3a7d20
Federico Ramos <federico.g.ramos@gmail.com>
2025-08-04 22:17:36 -0700

    solo acomodar llaves

--- keyPin/20250428 0057/TEMPLATE_kPin_aliases.h
+++ keyPin/20250428 0057/TEMPLATE_kPin_aliases.h
@@ -12,16 +12,15 @@
 //*****************************************************************************
 // ALIAS
 // LO UNICO QUE NECESITA PARA ESTABLECER EL ALIAS, ES QUE EL ALIAS SEA COINCIDENTE CON EL NRO DE ELEMENTO DEL ARRAY CON EL QUE SE INVOCA EL DRIVER DEL PULSADOR
-enum e_kPinAlias
-    {
+enum e_kPinAlias {
 	P0N,
 	P1N,
 	P0P,
 	IN_FN,
 	IN_FP,
 	KPIN_CANT
-    };
-//*****************************************************************************
+};
+
 
 
 #endif//__KPIN_ALIASES_H
