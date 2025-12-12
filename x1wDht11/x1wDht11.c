22a5059a2563069aa3881e348333d6b67d4fc40b
Federico Ramos <federico.g.ramos@gmail.com>
2025-12-12 12:34:26 -0800

    debugueando comunicacion

--- x1wDht11/x1wDht11.c
+++ x1wDht11/x1wDht11.c
@@ -72,10 +72,32 @@ void x1wDht11_init(void) {
  * 0 -- starting condition is bus = hi.
  * 1 -- uc sends start signal (pulls down voltage for at least 18ms).
  * 2 -- uc will pull up voltage and wait 20-40us for DHT’s response.
+ * 3 -- dht Responses to uc (low-voltage-level response signal, for 80us).
+ * 4 -- dht pulls up voltage and keeps it for 80us.
+ * 5 -- bit start = 0. Dht sends data to uc. Every bit of data begins with 50us 
+ *      low-voltage-level.
+ * 6 -- bit end = hi. The length of the following high-voltage-level signal dete
+ *      rmines whether data bit is "0" (26-28us) or "1" (70us).
+ * 
+ *                          |<---- 1 bit of data ---->|      |    last   |
+ *                                  |<- 26-28us = 0 ->|      |<-- data ->|
+ *                                  |<- 70us = 1 ---->|      |    bit    |
+ * _____      ____      ____  dht    _________________  ...__      ______
+ *      |    |    |    |    | bit   | dht bit         | other|    |      |
+ *      |    |    |    |    | start | logic state     | bits |    |      |
+ *      |____|    |____|    |_______| = f(width)      |_...  |____|      |_____
+ * start 18ms 40us 80us 80us 50us     26..70us                50us        50us
+ * bus   uc   uc   dht  dht  dht      dht bit logic                       dht
+ * hi         wait resp resp data     state dependent                     end
+ * [0]   [1]  [2]  [3]  [4]  [5]
  ******************************************************************************/
 
 char x1wDht11_get(void) {
-	char i, j, res, pulseTicks;
+	char i, j, res;
+	char dataBitWidthMeasurement;	// Acumulador para medir el ancho de 1 bit e
+									// n el bus de comunicación. Los bits de dat
+									// os que da el dht11 tienen valor logico de
+									// 0 o 1 dependiendo del ancho.
 
 	// Communication step 1.
 	X1W_DHT11_OUTPUT_ZERO();
@@ -85,39 +107,65 @@ char x1wDht11_get(void) {
 	X1W_INPUT_HI_Z();
 	DELAY_US(40); // 40us.
 
+	// Communication step 3.
 	res = X1W_DHT11_PIN;
 	
 	if(res == 0) {
-		DHT11_WAIT_FP();
-		DHT11_WAIT_FN(); //luego de esto, empieza transmision de datos
 
-		#if	defined(_12F683)// FGR: COMPILACION CONDICIONAL
+		DHT11_WAIT_FP();	// Blocking. El step 3 finaliza cuando dht pulls-up 
+							// voltage.
+
+		// Communication step 4. Dht acaba de hacer 0 => 1. Step 4 dura lo que d
+		// ht mantiene data = 1.
+		DHT11_WAIT_FN();	// Blocking. Luego de esto, comienza transmision de 
+							// datos.
+
+		// Communication step 5: ahora data = 0 durante 50us.
+
+		//TO-DO: creo que esto esta al pepe, porque adentro del loop ya hago el 
+		// mini "habilito deshabilito".
+		// Deshabilita todas las interrupciones porque:
+		// -- los tiempos son justos, no quiero que me agarre dentro de una inte
+		//    rrupción cuando haga el flanco positivo (q señala q debo comenzar 
+		//    a medir el tiempo).
+		// -- por el momento, por como está implementado, no le da el tiempo par
+		//    a atenderlas y al mismo tiempo reconocer el estado logico segun 26
+		//    us vs 70us.
+		#if	defined(_12F683) // Compilacion condicional.
 			GIE = 0;
 		#else
 			INTCONbits.GIE = 0;
-		#endif// FGR: COMPILACION CONDICIONAL
+		#endif
 
 		for(i = 0; i < DHT11_RESPONSE_BYTES; i++) {
 			dht11_mem[i] = 0;
 			for(j = 0; j < 8; j++) {
-				pulseTicks = 0;
+				dataBitWidthMeasurement = 0;
 
-				#if	defined(_12F683)// FGR: COMPILACION CONDICIONAL
+				// -- Cuidado: tengo 50us para atender interrupciones. Si me pas
+				// o, se jode todo.
+				// -- Deshabilita todas las interrupciones porque por el momento
+				// por como está implementado, no le da el tiempo para atenderla
+				// s y al mismo tiempo reconocer el estado logico segun 26us vs 
+				// 70us.
+				#if	defined(_12F683) // Compilacion condicional.
 					GIE = 1;
 					GIE = 0;
 				#else
 					INTCONbits.GIE = 1;
 					INTCONbits.GIE = 0;
-				#endif// FGR: COMPILACION CONDICIONAL
+				#endif
 
+				// Step 6: busco el momento en el que 0 -> 1 para comenzar a med
+				// ir el ancho de pulso que me dará el valor lógico del bit.
 				DHT11_WAIT_FP();
 
 				do {
 					DELAY_US(10.5);
-					pulseTicks++;
+					dataBitWidthMeasurement++;
 				} while(X1W_DHT11_PIN == 1);
 	
-				if(pulseTicks>3) dht11_mem[i] = (0x80 >> j);
+				if(dataBitWidthMeasurement > 3) dht11_mem[i] |= (0x80 >> j);
 			}
 		}
 
--- x1wDht11/x1wDht11.h
+++ x1wDht11/x1wDht11.h
@@ -1,88 +1,86 @@
-//******************************************************************************
-// ARCHIVO:     x1w_lib.h
-// AUTOR:       Federico Ramos
-// ZLT. MODIF:  10-12-2021 1507
-//******************************************************************************
+/*******************************************************************************
+ * @file x1wDht11.h
+ * @author Federico Ramos <federico.g.ramos@gmail.com>
+ * @version 20251212 1723
+ * @note Para lib manejo dht11.
+ ******************************************************************************/
 
 
-#ifndef __X1_WIRE_DHT11_LIB_H
-#define __X1_WIRE_DHT11_LIB_H
+#ifndef __X1W_DHT11_H
+#define __X1W_DHT11_H
 
 
-//******************************************************************************
-//PARA DHT11
-//******************************************************************************
+#include <x1wDht11_settings.h>
+
+
+/*******************************************************************************
+ * @brief Constants
+ ******************************************************************************/
+
+int nCyclesDelay; // Delay aprox 0.5us por cada unidad de delay.
+
+
+/*******************************************************************************
+ * @brief Constants
+ ******************************************************************************/
+
 #define DHT11_RESPONSE_BYTES 5
-#define DHT11_WAIT_FP()   while(X1W_DHT11_PIN == 0);
-#define DHT11_WAIT_FN() while(X1W_DHT11_PIN == 1);
+
+
+/*******************************************************************************
+ * @brief External declarations
+ ******************************************************************************/
 
 extern char dht11_mem[DHT11_RESPONSE_BYTES];
+
+
+/*******************************************************************************
+ * @brief Macros
+ ******************************************************************************/
+
+#define DHT11_WAIT_FP() while(X1W_DHT11_PIN == 0);
+#define DHT11_WAIT_FN() while(X1W_DHT11_PIN == 1);
 
 // Checksum es la suma de los 4 bytes enviados, pero como el dht11 tiene las par
 // tes decimales = 0, entonces estas suman 0. Es el dht22 (creo) que envia los 4
 // bytes útiles.
 #define DHT_XX_CHECKSUM() (dht11_mem[0] + dht11_mem[2] == dht11_mem[4])
 
-
-
-/////////////////////
-
-
+// Ojo, dependiente de la frecuencia de funcionamiento: mejorar o hacerlo config
+//urable o poner una tabla con las escalas.
+#define DELAY_US(n) {                                                          \
+	nCyclesDelay = ((int) (n / 1.084375));                                     \
+	while(--nCyclesDelay);                                                     \
+	}
 
 
 //******************************************************************************
 //PARA DS18B20
+//
+// TO-DO: esto eliminarlo.
 //******************************************************************************
-#define X1W_DS18B20_SKIP 0xCC
-#define X1W_DS18B20_CONVERT 0x44
-#define X1W_DS18B20_READTEMP 0xBE
+
+///////////////////////////////////////////////////#define X1W_DS18B20_SKIP 0xCC
+////////////////////////////////////////////////#define X1W_DS18B20_CONVERT 0x44
+///////////////////////////////////////////////#define X1W_DS18B20_READTEMP 0xBE
 
 
 //******************************************************************************
 //DEFINICION DE USUARIOS
 //******************************************************************************
-//volatile bit X1_WIRE_0 @ ((unsigned)&GPIO*8)+5;
-//volatile bit X1_WIRE_0_DDR @ ((unsigned)&GPIO*8)+5;
-//volatile bit X1W_DHT11_PIN @ ((unsigned)&PORTA*8)+3;
-//volatile bit X1W_DHT11_PIN_DDR @ ((unsigned)&TRISA*8)+3;
+/////////////////////////////////volatile bit X1_WIRE_0 @ ((unsigned)&GPIO*8)+5;
+/////////////////////////////volatile bit X1_WIRE_0_DDR @ ((unsigned)&GPIO*8)+5;
+////////////////////////////volatile bit X1W_DHT11_PIN @ ((unsigned)&PORTA*8)+3;
+////////////////////////volatile bit X1W_DHT11_PIN_DDR @ ((unsigned)&TRISA*8)+3;
+
+/////////////////////////////////volatile bit X1_WIRE_0 @ ((unsigned)&GPIO*8)+5;
+///////////////////////////volatile bit X1_WIRE_0_DDR @ ((unsigned)&TRISIO*8)+5;
+
+////////////////////////////////////////////////////////#define X1_WIRE_BUFLEN 8
 
 
-//volatile bit X1_WIRE_0 @ ((unsigned)&GPIO*8)+5;
-//volatile bit X1_WIRE_0_DDR @ ((unsigned)&TRISIO*8)+5;
-
-#include <x1wDht11_settings.h>
-
-
-#define X1_WIRE_BUFLEN 8
-
-
-
-//daly aprox 0.5us por cada unidad de delay
-int nCyclesDelay;
-
-
-#define DELAY_US(n)\
-	{\
-	nCyclesDelay=((int)(n/1.084375));\
-	while(--nCyclesDelay);\
-	}
-
-
-
-//******************************************************************************
-// DECLARACISN DE FUNCIONES
-//char x1w_rst(void);
-//void x1w_write_bit(char b);
-//char x1w_read_bit(void);
-//void x1w_write_byte(char data);
-//char x1w_read_byte(void);
-//char OWTouchByte(char data);
-//void OWBlock(unsigned char *data, char data_len);
-//char ReadPageMAC(void);
 char x1wDht11_get(void);
-//******************************************************************************
-
- void x1wDht11_init(void);
+void x1wDht11_init(void);
 
 
-#endif // __X1_WIRE_DHT11_LIB_H
+#endif // __X1W_DHT11_H
